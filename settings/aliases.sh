a10ea7fd141ee1729a278c711dc061d9d13f9ebf
pawurb <p.urbanek89@gmail.com>
2013-08-06 16:14:43 +0200

    added some aliases

--- settings/aliases.sh
+++ settings/aliases.sh
@@ -9,11 +9,13 @@ alias rs='rspec'
 alias zs='zeus start'
 alias zec='zeus console'
 alias zecs='zeus console --sandbox'
+alias rw='cd ~/Programming/owf_ekobilet'
 
 alias eed1='redis-server /usr/local/etc/redis.conf && rvmsudo tunnels' ##Execute EkoBilet Development I
 alias eed2='QUEUES=* bundle exec rake environment resque:work' #Execute EkoBilet Development II
 alias eed3='rake resque:scheduler' #Execute EkoBilet Development III
 alias ep='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start' # Execute Postgres
+alias mss='mysql.server start' #MySql Start
 
 #Git
 alias gs='git status'
@@ -26,4 +28,11 @@ alias ..='cd ..'
 alias ...='cd .. && cd ..'
 alias pd='cd -' #Previous Directory
 alias dd='echo $PWD' #Display Directory
+alias la='ls -a' #List All
+alias ll='ls -al' #Long List
 alias h='history'
+alias c='clear'
+alias reload='source ~/.bashrc'
+  #overrides
+  alias grep='grep --color=auto'
+  alias mkdir='mkdir -pv'
--- settings/env.sh
+++ settings/env.sh
@@ -4,3 +4,4 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 
 #for RVM to work
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
