6b89a3786b668679f7b7eddf3ea75061136d7c32
pawurb <p.urbanek89@gmail.com>
2013-08-07 11:50:09 +0200

    wrapped in class and you can set options now

--- settings/scripts/google_translate.rb
+++ settings/scripts/google_translate.rb
@@ -2,21 +2,67 @@
 require 'net/http'
 require 'uri'
 
-uri = URI.parse 'http://translate.google.com/'
-
-p ARGV
-
-data = { tl: 'en', sl: 'pl' , text: 'gruba dupa siedzi w krzakach'}
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri)
-request.set_form_data(data)
-response = http.request(request)
+# result=$(curl -s -i --user-agent "" -d "sl=$source" -d "tl=$target" --data-urlencode "text=$1" http://translate.google.com)
 
 
-result = (response.body.scan /(TRANSLATED_TEXT=')(.*?)(')/)
-p result
-result = result.pop
-p result[1]
+class Translator
+
+  def initialize(data = {})
+    p ARGV
+    set_data data
+  end
+
+  def call
+    response = send_request
+    p parse(response)
+  end
+
+  private
 
 
- # result=$(curl -s -i --user-agent "" -d "sl=$source" -d "tl=$target" --data-urlencode "text=$1" http://translate.google.com)
+    def default_data
+      {
+        source_language: 'pl',
+        target_language: 'en',
+        text: 'gruba dupa siedzi w krzakach'
+      }
+    end
+
+    def set_data data
+      @data = default_data
+      @data = @data.merge data
+      url = "http://translate.google.com/?tl=#{@data[:target_language]}&sl=#{@data[:source_language]}"
+      @uri = URI.parse url
+    end
+
+    def send_request
+      http = Net::HTTP.new(@uri.host, @uri.port)
+      request = Net::HTTP::Post.new(@uri.request_uri)
+      request.set_form_data(@data)
+      response = http.request(request)
+    end
+
+    def parse response
+      result = response.body.scan /(TRANSLATED_TEXT=')(.*?)(')/
+      result.pop[1]
+    end
+
+end
+
+
+
+
+user_options = {}
+user_options[:source_language] = ARGV[0]
+user_options[:target_language] = ARGV[1]
+# user_options[:text] = ARGV[2]
+
+
+
+Translator.new(user_options).call
+
+
+
+
+
+
