b282791266f59d8c4f473c1c6371046d5b688103
pawurb <p.urbanek89@gmail.com>
2013-08-07 11:01:40 +0200

    translations work now but without custom input

--- settings/scripts/google_translate.rb
+++ settings/scripts/google_translate.rb
@@ -4,17 +4,19 @@ require 'uri'
 
 uri = URI.parse 'http://translate.google.com/'
 
-http = Net::HTTP.new(uri.host, uri.port)
-response = http.request(Net::HTTP::Get.new(uri.request_uri))
-result = response.body.scan /(TRANSLATED_TEXT=')(.*?)(')/
-result.shift
-p result.join
+p ARGV
 
-  #data = {tl: :en, sl: :pl , word: 'dupa'} alal 5 8
- # def send_post_reqest
- #      http = Net::HTTP.new(@uri.host, @uri.port)
- #      request = Net::HTTP::Post.new(@uri.request_uri)
- #      request.set_form_data(@data)
- #      response = http.request(request)
- #      response.body
- #    end
+data = { tl: 'en', sl: 'pl' , text: 'gruba dupa siedzi w krzakach'}
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri)
+request.set_form_data(data)
+response = http.request(request)
+
+
+result = (response.body.scan /(TRANSLATED_TEXT=')(.*?)(')/)
+p result
+result = result.pop
+p result[1]
+
+
+ # result=$(curl -s -i --user-agent "" -d "sl=$source" -d "tl=$target" --data-urlencode "text=$1" http://translate.google.com)
