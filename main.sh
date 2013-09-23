be4b4678a677325285ce3abfc6d4b6ef4a24970c
pawurb <p.urbanek89@gmail.com>
2013-09-23 11:28:59 +0200

    unify structure

--- main.sh
+++ main.sh
@@ -3,5 +3,4 @@ export BASH_CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #load config files
 source $BASH_CONFIG_DIR/settings/git/main.sh
-source $BASH_CONFIG_DIR/settings/shell/aliases.sh
-source $BASH_CONFIG_DIR/settings/shell/config.sh
+source $BASH_CONFIG_DIR/settings/shell/main.sh
---
+++ settings/shell/main.sh
@@ -0,0 +1,2 @@
+source $BASH_CONFIG_DIR/settings/shell/aliases.sh
+source $BASH_CONFIG_DIR/settings/shell/config.sh
