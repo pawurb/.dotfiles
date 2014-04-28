08dc338319ea0a69ec567cae34b359311f3e7c00
pawurb <p.urbanek89@gmail.com>
2014-04-28 12:58:03 +0200

    chrome executalbe and hide minimap by default

--- settings/shell/aliases.sh
+++ settings/shell/aliases.sh
@@ -101,3 +101,4 @@ alias readArd='tail -f /dev/tty.usbmodem1411'
 
 #smoopay stuff
 alias 'rsb'='java -XstartOnFirstThread -jar /Users/user/Programming/java-stuff/swt-app.jar' #Run Smoopay Browser
+alias 'ichr'='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' --allow-running-insecure-content #Insecure Chrome
