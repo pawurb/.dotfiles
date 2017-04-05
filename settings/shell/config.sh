0bc66a9d4d23e21d1bc6a2e5b0dfe17281ed29f4
pawurb <p.urbanek89@gmail.com>
2017-04-05 13:27:23 +0200

    rvm config

--- settings/shell/config.sh
+++ settings/shell/config.sh
@@ -22,10 +22,6 @@ source $ZSH/oh-my-zsh.sh
 
 export PATH=$PATH:/home/pablo/.rvm/gems/ruby-2.0.0-p247/bin:/home/pablo/.rvm/gems/ruby-2.0.0-p247@global/bin:/home/pablo/.rvm/rubies/ruby-2.0.0-p247/bin:/home/pablo/.rvm/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/home/pablo/.rvm/bin
 
-#for RVM
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-
 #add karma executable
 export PATH="$PATH:/usr/local/lib/node_modules/karma/bin"
 
@@ -38,5 +34,10 @@ export EDITOR=vi
 #Java?
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_111.jdk/Contents/Home"
 
-# Load RVM into a shell session *as a function*
+export NVM_DIR="/Users/pablo/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+# Load RVM into a shell session *as a function*
+
+
--- settings/shell/main.sh
+++ settings/shell/main.sh
@@ -1,3 +1,4 @@
 source ~/.dotfiles/settings/shell/config.sh
 source ~/.dotfiles/settings/shell/aliases.sh
 source ~/.dotfiles/settings/shell/theme.sh
+
