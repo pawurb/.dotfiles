bd94d63b98922e0dcb2aed547c14172bb38fade0
pawurb <p.urbanek89@gmail.com>
2018-05-06 22:19:09 +0200

    addblock

---
+++ block.sh
@@ -0,0 +1,16 @@
+echo '##############  block ###########
+0.0.0.0 www.facebook.com
+:: www.facebook.com
+0.0.0.0 twitter.com
+:: twitter.com
+0.0.0.0 www.messenger.com
+:: www.messenger.com
+0.0.0.0 www.reddit.com
+:: www.reddit.com
+0.0.0.0 youtube.com
+:: youtube.com
+0.0.0.0 www.producthunt.com
+:: www.producthunt.com
+
+#############' >> /etc/hosts
+
