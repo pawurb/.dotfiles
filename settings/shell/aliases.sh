f1edaade6ccb7f758d3ae969b0c4213552a56aa5
pawurb <p.urbanek89@gmail.com>
2018-07-19 12:14:21 +0200

    git git

--- settings/shell/aliases.sh
+++ settings/shell/aliases.sh
@@ -54,7 +54,9 @@ alias gpr='git remote update --prune' # Git Prune: update list of remote branche
 alias gbr='git branch -a' #Git Branches
 alias gms='git checkout master' # Git Masta
 alias gca='git commit --amend'
+alias grc='git rebase --continue'
 alias gcm='git commit'
+alias gch='git checkout'
 alias grs='git reset --soft HEAD~1'
 alias gl="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gstat="git log --author="pawurb" --oneline --shortstat"
